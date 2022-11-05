4b729088814431ed42e242833e423f3812c6d182
Luiza Valentini <luizadaravalentini@gmail.com>
2022-11-05 17:14:43 -0300

    finalizando a home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../../components/header'
+import Body from '../../components/Body'
+
 
 const Home = () => {
   return (
-    <Header />
+    <>
+     <Header />
+     <Body />
+    </>
   )
 }
-
 export default Home
