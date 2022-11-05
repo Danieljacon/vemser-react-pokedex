79d97f6fe702047b9ece1635e2270273b3279550
Danieljacon <danielcamillo2020@gmail.com>
2022-11-05 17:10:08 -0300

    Corrigido o tamanho da imagem no mobile

--- src/pages/error/index.jsx
+++ src/pages/error/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Error.module.css";
-import image404 from "../../assets/404.png";
+import image404 from "../../assets/error404.png";
 import imageRocket from "../../assets/teamRocket.png";
 import { Link } from "react-router-dom";
 
@@ -9,7 +9,6 @@ const Error = () => {
     <div className={styles.container}>
       <div className={`container ${styles.box}`}>
         <img className={styles.image404} src={image404} alt="Error 404" />
-        <img className={styles.imageRocket} src={imageRocket} alt="Rocket team" />
         <p>The rocket team has won this time.</p>
         <Link to={"/"}>Return</Link>
       </div>
