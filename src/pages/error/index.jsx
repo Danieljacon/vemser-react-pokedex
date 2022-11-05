c02089b4d85c3fdf89280b43ae4b98857a8a80b3
Danieljacon <danielcamillo2020@gmail.com>
2022-11-05 17:01:11 -0300

    Adicionada a página de erro 404

--- src/pages/error/index.jsx
+++ src/pages/error/index.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import styles from "./Error.module.css";
+import image404 from "../../assets/404.png";
+import imageRocket from "../../assets/teamRocket.png";
+import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div>Error</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={`container ${styles.box}`}>
+        <img className={styles.image404} src={image404} alt="Error 404" />
+        <img className={styles.imageRocket} src={imageRocket} alt="Rocket team" />
+        <p>The rocket team has won this time.</p>
+        <Link to={"/"}>Return</Link>
+      </div>
+    </div>
+  );
+};
 
-export default Error
+export default Error;
