c71f9eeb6e9db35e1e9f00c7c2a041bf7c51e6cb
Danieljacon <danielcamillo2020@gmail.com>
2022-11-06 02:32:59 -0300

    adicionado menu hamburguer

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,20 +1,51 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styles from "./Header.module.css";
 import logo from "../../assets/logo.png";
 import HeaderLink from "../HeaderLink";
+import {ReactComponent as IconHamburguer} from "../../assets/i-hamb.svg";
+import {ReactComponent as IconClose} from "../../assets/x-solid.svg";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+  const menu = useRef(null);
+  const toggleMenu = () => {
+    setOpen(!open);
+
+    if (open) {
+      menu.current.className = styles.closed;
+    } else {
+      menu.current.className = styles.open;
+    }
+  };
+
+  useEffect(() => {
+    menu.current.className = styles.closed;
+    const links = document.querySelectorAll("a");
+    links.forEach((link) => {
+      link.addEventListener("click", () => {
+        menu.current.className = styles.closed;
+        setOpen(false);
+      });
+    });
+  }, []);
+
   return (
     <>
       <div className={styles.header}>
         <div className={styles.teste}>
           <img className={styles.logo} src={logo} alt="logo" />
-          <nav className={styles.nav}>
+          <nav className={styles.nav} ref={menu}>
             <HeaderLink to="/">Home</HeaderLink>
             <HeaderLink to="/PokeDex">Pokédex</HeaderLink>
             <HeaderLink to="/Legendaries">Legendaries</HeaderLink>
           </nav>
-          <div></div>
+          <button className={styles.menuBtn} onClick={toggleMenu}>
+            {open ? (
+             "⤫"
+            ) : (
+              "☰"
+            )}
+          </button>
         </div>
       </div>
     </>
