51f3ec6bb5a77560e610590ec6b33fb33f26a222
Danieljacon <danielcamillo2020@gmail.com>
2022-11-06 01:04:09 -0300

    Corrigida a responsividade

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -1,23 +1,30 @@
 import React from "react";
-import banner from "../../assets/banner.png"
+import { Link } from "react-router-dom";
+import banner from "../../assets/banner.png";
 import style from "./Body.module.css";
 
-
 const Body = () => {
-  return   <>
-             <div className={style.corpo}>
-              <div className={style.sec}>
-                <p className={style.find}><strong>Find</strong> All Your favorite <strong>Pokemon</strong></p>
-                <p className={style.type}>You can know the type of Pokemon, its strengths, disadvantages and abilities</p>
-                <button className={style.botao}>See pokemons</button>
-                <p className={style.make}>Make with ❤️ for the PokéSpartans team Platzi Master</p>
-              </div>
-              <div className={style.secti}>
-              <img className={style.banner} src={banner} alt="logo" />  
-              <p className={style.team}>Ours Team</p>
-              </div>
-              </div>  
-  </>
+  return (
+    <>
+      <div className={style.corpo}>
+        <div className={`container ${style.corpo}`}>
+          <div className={style.sec}>
+            <p className={style.find}>
+              <strong>Find</strong> all your favorite <strong>Pokémon</strong>
+            </p>
+            <p className={style.type}>
+              You can know the type of Pokemon, its strengths, disadvantages and
+              abilities
+            </p>
+            <Link to={"/pokedex"} className={style.botao}>See pokemons</Link>
+          </div>
+          <div className={style.secti}>
+            <img className={style.banner} src={banner} alt="logo" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Body;
