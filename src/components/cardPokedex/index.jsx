8c3a75278a69eee557863e361593470570b29fa7
Danieljacon <danielcamillo2020@gmail.com>
2022-11-05 22:47:47 -0300

    Corrigido a pesquisa e a responsividade dos cards

--- src/components/cardPokedex/index.jsx
+++ src/components/cardPokedex/index.jsx
@@ -1,24 +1,16 @@
 
 import styles from '../cardPokedex/cardPokedex.module.css'
 
-const CardPokedex = ({poke, index}) => {
-    // const [cor, setCor] = useState([]);
-    // let id = index.id;
-    // const getPokemon = async () => {
-    //     const response = await fetch(`https://pokeapi.co/api/v2/pokemon-color/${id}/`);
-    //     const data = await response.json();
-    //     console.log(id);
-    //     return setCor(data.results);
-    //   };
-
-    //   getPokemon()
-
+const CardPokedex = ({poke}) => {
+    const pokeImage = poke.url.split('/');
+    const pokeId = pokeImage[pokeImage.length - 2];
+    const pokeImg = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeId}.png`;
     return (
         <div className={styles.card}>
             <div  className={styles.item}>
                 <h4>{poke.name}</h4>
             </div>
-            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${index + 1}.png`} alt="" />
+            <img src={`${pokeImg}`} alt="" />
         </div>
 
     )
--- src/pages/pokedex/index.jsx
+++ src/pages/pokedex/index.jsx
@@ -1,53 +1,61 @@
-import { useEffect, useState } from 'react';
-import React from 'react'
-import CardPokedex from '../../components/cardPokedex'
-import styles from '../pokedex/pokedex.module.css'
+import { useEffect, useState } from "react";
+import React from "react";
+import CardPokedex from "../../components/cardPokedex";
+import styles from "../pokedex/pokedex.module.css";
 import { useForm } from "react-hook-form";
-
 
 const Pokedex = () => {
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
   const [poke, setPoke] = useState([]);
   const [pokeSearch, setPokeSearch] = useState([]);
 
-
   useEffect(() => {
     const fetchApi = async () => {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`)
-      const data = await response.json()
-      return setPoke(data.results)
-    }
-    fetchApi()
-  }, [])
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon?limit=151`
+      );
+      const data = await response.json();
+      return setPoke(data.results);
+    };
+    fetchApi();
+  }, []);
 
   const onSearch = (e) => {
     const name = e.name;
-    const pokeNome = poke.filter((parm) => parm.name.includes(name))
-    setPokeSearch(pokeNome);
-
-  }
+    const search = poke.filter((item) =>
+      item.name.includes(name.toLowerCase())
+    );
+    setPokeSearch(search);
+  };
 
   return (
-
-    <div className='container'>
+    <div className="container">
       <div className={styles.texto}>
-        <p>151 <span>Pokemons</span> for you choose you favorite</p>
+        <p>
+          151 <span>Pokemons</span> for you choose you favorite
+        </p>
       </div>
 
-      <form onSubmit={handleSubmit(onSearch)} >
-        <input type='text' placeholder='Encontre seu pokemom' {...register('name')} />
-        <input type="submit" value="Pesquisar" />
+      <form onChange={handleSubmit(onSearch)}>
+        <input
+          className={styles.input}
+          type="search"
+          placeholder="Encontre seu Pokémon"
+          {...register("name")}
+        />
       </form>
 
       <div className={styles.pokedex}>
-        {poke.map((pokemon, index) => {
-          return <CardPokedex index={index} poke={pokemon} key={index} />
-        })}
-
+        {pokeSearch.length > 0
+          ? pokeSearch.map((pokemon, index) => {
+              return <CardPokedex index={index} poke={pokemon} key={index} />;
+            })
+          : poke.map((pokemon, index) => {
+              return <CardPokedex index={index} poke={pokemon} key={index} />;
+            })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Pokedex
+export default Pokedex;
